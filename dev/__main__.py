d24905671e2430fecea33ec6474fe768765b2c2d
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2024-07-10 20:07:31 +0300

    refactor(test): simplify database plugin usage

--- dev/__main__.py
+++ dev/__main__.py
@@ -6,13 +6,13 @@ from sqlalchemy import create_engine, Inspector
 from dev.scripts.db import create_dev_database, upgrade_migrations_to_head
 from dev.scripts.docker import up_docker_compose_with_detach_option
 from dev.utils.base_utils import get_tmp_dir_path
-from tests.utils.db import get_dev_database_url
+from tests.plugins.database import get_database_url
 
 
 def perform_dev_deploy():
     """ todo """
     up_docker_compose_with_detach_option()
-    database_url = get_dev_database_url(dbname_suffix=False)
+    database_url = get_database_url()
     print(f"database URL: {database_url}")
 
     create_dev_database(database_url)
--- tests/plugins/database.py
+++ tests/plugins/database.py
@@ -1,23 +1,46 @@
 """
 Database plugin
 """
+from random import choice
+from string import ascii_uppercase, digits
 from typing import Callable
 
 import pytest
 from alembic.config import Config as AlembicConfig
 from sqlalchemy_utils import create_database, drop_database
 
-from settings import DB_PATH, ALEMBIC_INI_PATH
-from tests.utils.db import get_dev_database_url
+from settings import config, ALEMBIC_INI_PATH, DB_PATH
+
+
+def get_database_url(enrich_dbname_with_suffix: bool = False) -> str:
+    """ Prepares test database URL from config
+
+    Args:
+        enrich_dbname_with_suffix: boolean reflecting need to add 'test' suffix for database name, by default False
+
+    Returns:
+        database URL as string
+    """
+    database_name = config.db.database_name
+    if enrich_dbname_with_suffix:
+        database_name += f"-test-{choice(ascii_uppercase + digits)}"
+    return '{driver_name}://{username}:{password}@{host}:{port}/{database}'.format(
+        driver_name=f"{config.db.driver}+{config.db.dialect}",
+        username=config.db.username,
+        password=config.db.password,
+        host=config.db.host,
+        port=config.db.port,
+        database=database_name,
+    )
 
 
 @pytest.fixture(scope='session')
 def _test_database_url() -> str:
-    """ Fixture. Prepares database URL for testing
+    """ Fixture. Provides database URL for testing
 
     Note: random string adds for the database name
     """
-    return get_dev_database_url()
+    return get_database_url(enrich_dbname_with_suffix=True)
 
 
 @pytest.fixture(scope='session')
